4fe19b6c8b6a179aee88f218eb8caf54de4d0362
Angie <103882157+anakela@users.noreply.github.com>
2022-08-13 22:01:25 -1000

    Updated the product-routes.js and tag-routes.js API route files.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const allProducts = await Product.findAll({
-      include: [{ model: Category }, { model: Tag , through: ProductTag}]
+      include: [{ model: Category }, { model: Tag, through: ProductTag }]
     });
     res.status(200).json(allProducts);
   } catch (error) {
@@ -111,9 +111,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  
+  try {
+    const oneProduct = await Product.findByPk(req.params.id);
+
+    /*const deleteProduct = */
+    await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    res.json(oneProduct);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 module.exports = router;
